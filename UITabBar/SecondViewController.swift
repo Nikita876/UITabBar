aedf5c22071595cfc62a0e5f1f263f2e5b95cad3
nikita_kogolenok <nikiton17ns@gmail.com>
2021-04-13 10:36:35 +0300

    Initial Commit

---
+++ UITabBar/SecondViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.swift
+//  UITabBar
+//
+//  Created by Никита Коголенок on 30.12.20.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ UITabBar/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  UITabBar
+//
+//  Created by Никита Коголенок on 30.12.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
